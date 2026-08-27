f6931b6b916ef8b2f223083320af6d4df9746ed1
Nguyễn Hải Đăng <dang46315@gmail.com>
2026-08-27 14:42:45 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int chon;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("===== MENU =====");
+            Console.WriteLine("1. Bai A - Calculator");
+            Console.WriteLine("2. Bai B - Phuong trinh bac 2");
+            Console.WriteLine("3. Bai C - So nguyen to & Fibonacci");
+            Console.WriteLine("0. Thoat");
+            Console.WriteLine("================");
+
+            Console.Write("Nhap lua chon: ");
+            chon = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            switch (chon)
+            {
+                case 1:
+                    BaiA.Chay();
+                    break;
+
+                case 2:
+                    BaiB.Chay();
+                    break;
+
+                case 3:
+                    BaiC.Chay();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Thoat chuong trinh!");
+                    break;
+
+                default:
+                    Console.WriteLine("Lua chon khong hop le!");
+                    break;
+            }
+
+            if (chon != 0)
+            {
+                Console.WriteLine("\nNhan phim bat ky de quay lai...");
+                Console.ReadKey();
+            }
+
+        } while (chon != 0);
+    }
+}
---
+++ ConsoleApp2/bai3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class BaiC
+{
+    static bool IsPrime(int n)
+    {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static bool IsPerfectNumber(int n)
+    {
+        int tong = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (n % i == 0)
+                tong += i;
+        }
+
+        return tong == n;
+    }
+
+    public static void Chay()
+    {
+        Console.Write("Nhap N: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        if (IsPrime(n))
+            Console.WriteLine($"{n} la so nguyen to");
+        else
+            Console.WriteLine($"{n} khong la so nguyen to");
+
+        if (IsPerfectNumber(n))
+            Console.WriteLine($"{n} la so hoan hao");
+        else
+            Console.WriteLine($"{n} khong la so hoan hao");
+
+        Console.Write("Day Fibonacci: ");
+
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        Console.WriteLine();
+    }
+}
